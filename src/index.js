f8fb70b47c5e7df9b4ef3d1ce5aecfb3dee0f206
Dylan Ross Ludwick <dylan@devhominid.com>
2017-08-11 00:45:09 -0600

    add public directory and set as static

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 // Init app
 const app = express();
@@ -9,9 +10,27 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set public folder
+app.use(express.static(`${__dirname}/../public`));
+
 // Home route
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
+  res.render('index', {
+    title: 'Haikus'
+  });
+});
+
+// Add haiku route
+app.get('/haikus/add', (req, res) => {
+  res.render('add_haiku', {
+    title: 'Add Haiku'
+  });
 });
 
 // Start server
