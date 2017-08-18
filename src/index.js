a12c872cb5f7494a89abc147d1a014ddb619b763
Dylan Ross Ludwick <dylan@devhominid.com>
2017-08-18 16:27:18 -0600

    install express messages and validator, add middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser';
 import router from './router';
 import mongoose from 'mongoose';
 import Promise from 'mpromise';
+import expressValidator from 'express-validator';
+import flash from 'connect-flash';
+import session from 'express-session';
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/queue-haiku').then(
@@ -25,6 +28,39 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 // Parse application/json
 app.use(bodyParser.json());
+
+// Express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+// Express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 // Set public folder
 app.use(express.static(`${__dirname}/../public`));
