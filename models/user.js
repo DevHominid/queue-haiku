77e387c88d34d684e0ac8a53b33cd24f3ebf725e
Dylan Ross Ludwick <dylan@devhominid.com>
2017-08-21 12:59:29 -0600

    add user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Init user schema
+const userSchema = mongoose.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  username:{
+    type: String,
+    required: true
+  },
+  password:{
+    type: String,
+    required: true
+  }
+});
+
+const User = module.exports = mongoose.model('User', userSchema);
