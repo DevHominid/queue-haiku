355924ada5ee346e1921091f19cf65826eecab21
Dylan Ross Ludwick <dylan@devhominid.com>
2017-10-02 13:05:56 -0600

    fix flash message display bug

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,3 +1,13 @@
-$( '#icon-close' ).click(function() {
-  $( '.message-block' ).css( 'display', 'none' );
-});
+
+  $( '#icon-close' ).click(function() {
+    $( '.message-block' ).css( 'display', 'none' );
+  });
+
+function checkMessages() {
+  var messages = $( '.messages' );
+  console.log(messages);
+  if (messages.length === 0) {
+    $( '.message-block' ).css( 'display', 'none' );
+  }
+}
+checkMessages();
