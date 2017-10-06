1fcbe52932e5c39e2ce674b7930318ad057d8011
Dylan Ross Ludwick <dylan@devhominid.com>
2017-10-06 11:52:25 -0600

    add validation to edit haiku post route

--- src/routes/haikus.js
+++ src/routes/haikus.js
@@ -109,28 +109,52 @@ router.get('/edit/:id', controlAccess, (req, res) => {
 // Edit haiku POST route
 router.post('/edit/:id', (req, res) => {
 
+  // Validation
+  req.assert('title', 'Title is required').notEmpty();
+  req.assert('line1', 'Line 1 is required').notEmpty();
+  req.assert('line2', 'Line 2 is required').notEmpty();
+  req.assert('line3', 'Line 3 is required').notEmpty();
+
   // Sanitization
   const title = req.sanitize('title').escape().trim();
   const line1 = req.sanitize('line1').escape().trim();
   const line2 = req.sanitize('line2').escape().trim();
   const line3 = req.sanitize('line3').escape().trim();
 
-  let haiku = {};
-  haiku.title = req.body.title;
-  haiku.imageURL = req.body.imageURL;
-  haiku.author = req.user._id;
-  haiku.line1 = req.body.line1;
-  haiku.line2 = req.body.line2;
-  haiku.line3 = req.body.line3;
+  req.getValidationResult().then((result) => {
 
-  let query = {_id:req.params.id}
+    // Get errors
+    let errors = result.array();
 
-  Haiku.update(query, haiku)
-    .then(() => {
-      req.flash('success', 'Haiku updated!')
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
+    if (!result.isEmpty()) {
+      Haiku.findById(req.params.id)
+      .then(haiku => {
+        res.render('edit_haiku', {
+          title: 'Edit Haiku',
+          haiku: haiku,
+          errors: errors
+        });
+      })
+      .catch(err => console.log(err));
+    } else {
+      let haiku = {};
+      haiku.title = req.body.title;
+      haiku.imageURL = req.body.imageURL;
+      haiku.author = req.user._id;
+      haiku.line1 = req.body.line1;
+      haiku.line2 = req.body.line2;
+      haiku.line3 = req.body.line3;
+
+      let query = {_id:req.params.id}
+
+      Haiku.update(query, haiku)
+        .then(() => {
+          req.flash('success', 'Haiku updated!')
+          res.redirect('/haikus/'+req.params.id);
+        })
+        .catch(err => console.log(err));
+    }
+  });
 });
 
 // haiku DELETE route
