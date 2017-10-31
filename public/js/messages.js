65d16ae20b07d85e46aa72c181b06b133a6e39ba
Dylan Ross Ludwick <dylan@devhominid.com>
2017-10-31 14:35:14 -0600

    cleanup

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,13 +1,11 @@
 
 $( '.icon-close' ).click(function() {
-  console.log('clicked');
   $( '.error-wrapper ').css( 'display', 'none' );
   $( '.message-wrapper' ).css( 'display', 'none' );
 });
 
 function checkMessages() {
   var messages = $( '.messages' );
-  console.log(messages);
   if (messages.length !== 0) {
     $( '.message-wrapper' ).css( 'display', 'block' );
   }
